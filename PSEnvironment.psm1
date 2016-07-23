be474b257e829ba04678b5ddaa53b8d252528425
Martin Gill <martin@martinsgill.co.uk>
2016-07-23 14:17:50 +0200

    add basic tests and fix a number of methods impacted by get-envpath method change

--- PSEnvironment.psm1
+++ PSEnvironment.psm1
@@ -2,185 +2,200 @@
 
 function Get-EnvironmentVariable
 {
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory = $true,ValueFromPipeline = $true)]
-        [String]$Name,
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory = $true,ValueFromPipeline = $true)]
+    [String]$Name,
 
-        [Parameter(Mandatory = $false)]
-        [ValidateSet('process','machine','user')]
-        [String]$Scope = 'process'
-    )
+    [Parameter(Mandatory = $false)]
+    [ValidateSet('process','machine','user')]
+    [String]$Scope = 'process'
+  )
     
-    [Environment]::GetEnvironmentVariable($Name, $Scope)
+  [Environment]::GetEnvironmentVariable($Name, $Scope)
 }
 
 function Set-EnvironmentVariable
 {
-    [CmdletBinding(SupportsShouldProcess = $true)]
-    param(
-        [Parameter(Mandatory = $true,ValueFromPipeline = $true)]
-        [String]$Name,
+  [CmdletBinding(SupportsShouldProcess = $true)]
+  param(
+    [Parameter(Mandatory = $true,ValueFromPipeline = $true)]
+    [String]$Name,
 
-        [Parameter(Mandatory = $true,ValueFromPipeline = $true)]
-        [String]$Value,
+    [Parameter(Mandatory = $true,ValueFromPipeline = $true)]
+    [String]$Value,
 
-        [Parameter(Mandatory = $false)]
-        [ValidateSet('process','machine','user')]
-        [String]$Scope = 'process'
-    )
+    [Parameter(Mandatory = $false)]
+    [ValidateSet('process','machine','user')]
+    [String]$Scope = 'process'
+  )
     
-    Write-Verbose -Message "Setting '$Name' to '$Value' on '$Scope'"
-    if ($PSCmdlet.ShouldProcess("[$Scope]::$Name", $Value))
-    {
-        [Environment]::SetEnvironmentVariable($Name, $Value, $Scope)
-    }
+  Write-Verbose -Message "Setting '$Name' to '$Value' on '$Scope'"
+  if ($PSCmdlet.ShouldProcess("[$Scope]::$Name", $Value))
+  {
+    [Environment]::SetEnvironmentVariable($Name, $Value, $Scope)
+  }
 }
 
 function Get-EnvironmentPath
 {
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory = $false)]
-        [ValidateSet('process','machine','user')]
-        [String[]]$Scope
-    )
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory = $false)]
+    [ValidateSet('process','machine','user')]
+    [String[]]$Scope
+  )
     
-    if (-not $Scope) {
-        $Scope = @('process','machine','user')
-    }
+  if (-not $Scope) 
+  {
+    $Scope = @('process', 'machine', 'user')
+  }
 
-    foreach ($s in $Scope) {
-        $pathss = (Get-EnvironmentVariable -Name 'PATH' -Scope $s) -split ';'
-        foreach ($paths in $pathss) {
-            [pscustomobject]@{ Path = $paths; Scope = $s; Exists = Test-Path($paths) }
-        }
+  foreach ($s in $Scope) 
+  {
+    $pathss = (Get-EnvironmentVariable -Name 'PATH' -Scope $s) -split ';'
+    foreach ($paths in $pathss) 
+    {
+      [pscustomobject]@{
+        Path   = $paths
+        Scope  = $s
+        Exists = Test-Path($paths)
+      }
     }
+  }
 }
 
 function Repair-EnvironmentPath
 {
-    [CmdletBinding(SupportsShouldProcess = $true)]
-    param(
-        [Parameter(Mandatory = $false)]
-        [ValidateSet('process','machine','user')]
-        [String]$Scope = 'process'
-    )
-    if ($Scope -eq 'process')
-    {
-        Write-Warning -Message 'This will change current-process value only. This may not be what you intended; see -Scope'
-    }
+  [CmdletBinding(SupportsShouldProcess = $true)]
+  param(
+    [Parameter(Mandatory = $false)]
+    [ValidateSet('process','machine','user')]
+    [String]$Scope = 'process'
+  )
+  if ($Scope -eq 'process')
+  {
+    Write-Warning -Message 'This will change current-process value only. This may not be what you intended; see -Scope'
+  }
     
-    $verbose = ($PSCmdlet.MyInvocation.BoundParameters['Verbose'].IsPresent -eq $true)
+  $verbose = ($PSCmdlet.MyInvocation.BoundParameters['Verbose'].IsPresent -eq $true)
 
-    # Ensure unique paths only
-    $paths = Get-EnvironmentPath -Scope $Scope
-    $result = @()
-    foreach ($path in ($paths | Select-Object -ExpandProperty Path)) {
-      if ([string]::IsNullOrWhiteSpace($path)) {
-        Write-Verbose 'Found empty path, skipping'
-        if ($PSCmdlet.ShouldProcess($path, 'Skip empty path entry')) {
-          continue
-        }
-      }
-      
-      $path = $path.Trim()
-      if ($path -in $result) {
-        Write-Verbose 'Found duplicate path, skipping'
-        if ($PSCmdlet.ShouldProcess($path, 'Skip duplicate path entry')) {
-          continue
-        }
-      }
-
-      if (-not (Test-Path $path -PathType Container)) {
-        Write-Verbose 'Found invliad path, skipping'
-        if ($PSCmdlet.ShouldProcess($path, 'Skip invalid path entry')) {
-          continue
-        }
-      }
-      
-      $result += $path
-    }
-
-    if ($PSCmdlet.ShouldProcess("`n$($result -join "`n")`n", 'Update environment with paths'))
+  # Ensure unique paths only
+  $paths = Get-EnvironmentPath -Scope $Scope
+  $result = @()
+  foreach ($path in ($paths | Select-Object -ExpandProperty Path)) 
+  {
+    if ([string]::IsNullOrWhiteSpace($path)) 
     {
-        Set-EnvironmentVariable -Scope $Scope -Name PATH -Value ($result -join ';')
+      Write-Verbose -Message 'Found empty path, skipping'
+      if ($PSCmdlet.ShouldProcess($path, 'Skip empty path entry')) 
+      {
+        continue
+      }
     }
+      
+    $path = $path.Trim()
+    if ($path -in $result) 
+    {
+      Write-Verbose -Message 'Found duplicate path, skipping'
+      if ($PSCmdlet.ShouldProcess($path, 'Skip duplicate path entry')) 
+      {
+        continue
+      }
+    }
+
+    if (-not (Test-Path $path -PathType Container)) 
+    {
+      Write-Verbose -Message 'Found invliad path, skipping'
+      if ($PSCmdlet.ShouldProcess($path, 'Skip invalid path entry')) 
+      {
+        continue
+      }
+    }
+      
+    $result += $path
+  }
+
+  if ($PSCmdlet.ShouldProcess("`n$($result -join "`n")`n", 'Update environment with paths'))
+  {
+    Set-EnvironmentVariable -Scope $Scope -Name PATH -Value ($result -join ';')
+  }
 }
 
 function Test-EnvironmentPath
 {
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory = $true,ValueFromPipeline = $true)]
-        [String]$paths,
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory = $true,ValueFromPipeline = $true)]
+    [String]$path,
 
-        [Parameter(Mandatory = $false)]
-        [ValidateSet('process','machine','user')]
-        [String]$Scope = 'process'
-    )
+    [Parameter(Mandatory = $false)]
+    [ValidateSet('process','machine','user')]
+    [String]$Scope = 'process'
+  )
 
-    (Get-EnvironmentPath -Scope $Scope | Where-Object -FilterScript {
-            $_ -ieq $paths 
-    }).Count -gt 0
+  (Get-EnvironmentPath -Scope $Scope | Where-Object -FilterScript {
+      $_.path -ieq $path
+  }).Count -gt 0
 }
 
 function Add-EnvironmentPath
 {
-    [CmdletBinding(SupportsShouldProcess = $true)]
-    param(
-        [Parameter(Mandatory = $true,ValueFromPipeline = $true)]
-        [String]$paths,
+  [CmdletBinding(SupportsShouldProcess = $true)]
+  param(
+    [Parameter(Mandatory = $true,ValueFromPipeline = $true)]
+    [String]$paths,
 
-        [Parameter(Mandatory = $false)]
-        [ValidateSet('process','machine','user')]
-        [String]$Scope = 'process'
-    )
+    [Parameter(Mandatory = $false)]
+    [ValidateSet('process','machine','user')]
+    [String]$Scope = 'process'
+  )
     
 
-    $paths = $paths.TrimEnd('\')
-    if (!(Test-Path -Path $paths -PathType container))
-    {
-        throw 'Invalid Path'
-    }
+  $paths = $paths.TrimEnd('\')
+  if (!(Test-Path -Path $paths -PathType container))
+  {
+    throw 'Invalid Path'
+  }
 
-    $envPath = Get-EnvironmentPath -Scope $Scope
-    if (Test-EnvironmentPath -Scope $Scope -Path $paths)
-    {
-        throw 'Path already in PATH variable'
-    }
+  $envPath = Get-EnvironmentPath -Scope $Scope
+  if (Test-EnvironmentPath -Scope $Scope -Path $paths)
+  {
+    throw 'Path already in PATH variable'
+  }
     
-    $result = $envPath + $paths
-    Write-Verbose -Message "New Path: $($result -join ';')"
-    if ($PSCmdlet.ShouldProcess('PATH', 'Update Environment Variable'))
-    {
-        Set-EnvironmentVariable -Scope $Scope -Name PATH -Value ($result -join ';')
-    }
+  $result = $envPath + $paths
+  Write-Verbose -Message "New Path: $($result -join ';')"
+  if ($PSCmdlet.ShouldProcess('PATH', 'Update Environment Variable'))
+  {
+    Set-EnvironmentVariable -Scope $Scope -Name PATH -Value ($result -join ';')
+  }
 }
 
 function Remove-EnvironmentPath
 {
-    [CmdletBinding(SupportsShouldProcess = $true)]
-    param(
-        [Parameter(Mandatory = $true,ValueFromPipeline = $true)]
-        [String]$paths,
+  [CmdletBinding(SupportsShouldProcess = $true)]
+  param(
+    [Parameter(Mandatory = $true,ValueFromPipeline = $true)]
+    [String]$path,
 
-        [Parameter(Mandatory = $false)]
-        [ValidateSet('process','machine','user')]
-        [String]$Scope = 'process'
-    )
+    [Parameter(Mandatory = $false)]
+    [ValidateSet('process','machine','user')]
+    [String]$Scope = 'process'
+  )
     
-    $envPath = Get-EnvironmentPath -Scope $Scope
-    if (!(Test-EnvironmentPath -Scope $Scope -Path $paths))
-    {
-        throw 'Path not in PATH variable'
-    }
+  $envPath = Get-EnvironmentPath -Scope $Scope
+  if (!(Test-EnvironmentPath -Scope $Scope -Path ($paths | Select-Object -ExpandProperty Path)))
+  {
+    throw 'Path not in PATH variable'
+  }
 
-    $result = $envPath | Where-Object -FilterScript {
-        $_ -ine $paths 
-    }
-    if ($PSCmdlet.ShouldProcess('PATH', "remove: $paths"))
-    {
-        Set-EnvironmentVariable -Name PATH -Value ($result -join ';')
-    }
+  $result = $envPath | Where-Object -FilterScript {
+    $_ -ine $paths
+  }
+  if ($PSCmdlet.ShouldProcess('PATH', "remove: $paths"))
+  {
+    Set-EnvironmentVariable -Name PATH -Value ($result -join ';')
+  }
 }
+
---
+++ PSEnvironment.tests.ps1
@@ -0,0 +1,31 @@
+﻿# this is a Pester test file
+
+#region Further Reading
+# http://www.powershellmagazine.com/2014/03/27/testing-your-powershell-scripts-with-pester-assertions-and-more/
+#endregion
+#region LoadScript
+# load the script file into memory
+# attention: make sure the script only contains function definitions
+# and no active code. The entire script will be executed to load
+# all functions into memory
+Import-Module (Join-Path $PSScriptRoot PSEnvironment.psm1) -Force
+#endregion
+
+InModuleScope PSEnvironment {
+  Describe 'Test-EnvironmentPath' {
+    Context 'Path in environment' {
+      Mock Get-EnvironmentPath { @{ path = 'c:\path1\path1\' }, @{ path = 'c:\path2\path2\' }  }
+      It 'returns true' {
+        Test-EnvironmentPath -path 'c:\path1\path1'
+      }
+    }
+    
+    Context 'Path not in environment' {
+      Mock Get-EnvironmentPath { @{ path = 'c:\path1\path1\' }, @{ path = 'c:\path2\path2\' }  }
+      It 'returns false' {
+        Test-EnvironmentPath -path 'c:\path3\path3'
+      }
+    }
+  
+  }    
+}
